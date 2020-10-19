a32ec0d987aefa1a81e38b7c4375c89f91446e5c
Tav Hafner <23143092+haftav@users.noreply.github.com>
2020-10-18 21:49:38 -0600

    Removing unused code

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,55 +1,10 @@
 import React, {useState} from 'react';
-import Link from 'next/link';
-import {Flex, Box, Button, Text} from '@chakra-ui/core';
-import {signOut} from 'next-auth/client';
 
 import Session from 'components/Session';
 import MobileHeader from './MobileHeader';
 import DesktopHeader from './DesktopHeader';
 import {Session as SessionModel} from 'models';
-import CreateMenu from './CreateMenu';
 import CreateModal from './CreateModal';
-
-const ProfileLink = () => (
-  <Box marginLeft="45px">
-    <Link href="profile">
-      <a>
-        <Text fontSize="sm">Profile</Text>
-      </a>
-    </Link>
-  </Box>
-);
-
-const LoginLink = () => (
-  <Link href="/login">
-    <a>
-      <Button variant="outline" w="100px" m="0px 10px">
-        Log In
-      </Button>
-    </a>
-  </Link>
-);
-
-const RegisterLink = () => (
-  <Link href="/register">
-    <a>
-      <Button variant="outline" w="100px" m="0px 10px">
-        Sign Up
-      </Button>
-    </a>
-  </Link>
-);
-
-const LogoutLink = () => (
-  <Button
-    variant="outline"
-    w="100px"
-    m="0px 10px"
-    onClick={() => signOut({callbackUrl: process.env.NEXT_PUBLIC_CALLBACK_URL})}
-  >
-    Log Out
-  </Button>
-);
 
 interface HeaderProps {
   session: SessionModel;
