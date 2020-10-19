6655d661c00e32311644c5699897bbfaefa06018
Tav Hafner <23143092+haftav@users.noreply.github.com>
2020-10-18 19:48:38 -0600

    Almost finished with mobile menu

--- components/MobileHeader.tsx
+++ components/MobileHeader.tsx
@@ -3,14 +3,23 @@ import {
   Box,
   Flex,
   Button,
+  Divider,
   Drawer,
+  Text,
+  Link as ChakraLink,
   DrawerBody,
   DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
   DrawerContent,
   DrawerCloseButton,
+  Heading,
 } from '@chakra-ui/core';
+import Link from 'next/link';
+import {signOut} from 'next-auth/client';
+
+import Session from './Session';
+import {Session as SessionModel} from 'models';
 
 const MenuIcon = () => (
   <svg fill="#CBD5E0" width="24" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -28,14 +37,23 @@ const MobileHeader = () => {
   };
 
   return (
-    <Box height="75px" display={['block', 'block', 'none']} pos="relative">
+    <Box height="75px" display={['block', 'block', 'none']} pos="relative" overflow="auto">
+      <Link href="/">
+        <a>
+          <Box m="20px 20px">
+            <Text fontSize="2xl" fontWeight="bold">
+              My Application
+            </Text>
+          </Box>
+        </a>
+      </Link>
       <Flex
         h="50px"
         w="50px"
         pos="absolute"
         right="20px"
         top="20px"
-        justify="center"
+        justify="flex-start"
         align="center"
       >
         <Button
@@ -52,12 +70,63 @@ const MobileHeader = () => {
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton h="50px" w="50px" top="20px" right="20px" />
-          <DrawerHeader height="75px">Create your account</DrawerHeader>
-
-          <DrawerBody>lalala</DrawerBody>
-
+          <DrawerHeader height="75px">
+            <Link href="/">
+              <a>
+                <Heading>Songstack</Heading>
+              </a>
+            </Link>
+          </DrawerHeader>
+          <DrawerBody>
+            <Session.LoggedOut>
+              <Link href="/login">
+                <a>
+                  <Button variant="solid" w="100%" h="50px" fontSize="1.2em" m="10px auto">
+                    Log In
+                  </Button>
+                </a>
+              </Link>
+            </Session.LoggedOut>
+            <Session.LoggedOut>
+              <Link href="/register">
+                <a>
+                  <Button variant="solid" w="100%" h="50px" fontSize="1.2em" m="10px auto">
+                    Sign Up
+                  </Button>
+                </a>
+              </Link>
+            </Session.LoggedOut>
+            <Session.LoggedIn>
+              {(loadedSession: SessionModel) => (
+                <>
+                  <Heading as="h2" size="lg" fontWeight="400" marginY="15px">
+                    Welcome back, {loadedSession.user.name}!
+                  </Heading>
+                  <Divider />
+                  <Link href="profile">
+                    <a>
+                      <Text fontSize="1.2em" fontWeight="bold" marginY="15px">
+                        Profile
+                      </Text>
+                    </a>
+                  </Link>
+                </>
+              )}
+            </Session.LoggedIn>
+          </DrawerBody>
           <DrawerFooter>
-            <Button color="blue" isDisabled>Log Out</Button>
+            <Session.LoggedIn>
+              <Button
+                variant="outline"
+                w="100%"
+                h="50px"
+                m="10px auto"
+                fontSize="1.2em"
+                onClick={() => signOut({callbackUrl: process.env.NEXT_PUBLIC_CALLBACK_URL})}
+              >
+                Log Out
+              </Button>
+            </Session.LoggedIn>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
