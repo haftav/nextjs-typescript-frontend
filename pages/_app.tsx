c9872c4b1d6695bbf0a0856e7d1bc399320473c5
Tav Hafner <23143092+haftav@users.noreply.github.com>
2020-11-15 13:00:48 -0500

    Adding tags in head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,21 @@
+import React from 'react';
 import {ThemeProvider, ColorModeProvider, CSSReset} from '@chakra-ui/core';
 import {ReactQueryDevtools} from 'react-query-devtools';
 import {ReactQueryConfigProvider} from 'react-query';
 import {Provider} from 'next-auth/client';
-
-import React from 'react';
 import {AppProps} from 'next/app';
+import Head from 'next/head';
 
 const queryConfig = {queries: {refetchOnWindowFocus: false}};
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <Provider session={pageProps.session}>
+      <Head>
+        <html lang="en" />
+        <meta name="Description" content="Keep track of your musical progress."></meta>
+        <title>Songstack</title>
+      </Head>
       <ReactQueryConfigProvider config={queryConfig}>
         <ThemeProvider>
           <ColorModeProvider value="dark">
